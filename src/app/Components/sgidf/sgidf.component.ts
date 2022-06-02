5d9a2297278b39d6db1ab91c617d8484e329de05
bouthayna <bouthayna.djebali@sofrecom.com>
2022-06-02 09:09:50 +0100

    creation identity

--- src/app/Components/sgidf/sgidf.component.ts
+++ src/app/Components/sgidf/sgidf.component.ts
@@ -23,77 +23,81 @@ interface idaasForm {
 export class SgidfComponent implements OnInit, OnDestroy {
 
   //declare subscriptions
-  createIdentityElementSbs =new Subscription
+  createIdentityElementSbs = new Subscription
 
-  error:boolean=false
-  errorMsg:string=""
-  dragPosition = {x: 0, y: 0};
-  line=[]
-  attachedNumber:number=1
-  sgidfForms:idaasForm[]=[
-    {name:"get Identifier", value:"getIdentifierElement"},
-    {name:"create Identifier", value:"createIdenifierElement"},
-    {name:"get Identity", value:"getIdentityElement"},
-    {name:"get contact identifier", value:"getContactIdentifierElement"},
-    {name:"update identity password", value:"updateIdentityPwdElement"}
+  error: boolean = false
+  errorMsg: string = ""
+  dragPosition = { x: 0, y: 0 };
+  line = []
+  attachedNumber: number = 1
+  sgidfForms: idaasForm[] = [
+    { name: "get Identifier", value: "getIdentifierElement" },
+    { name: "create Identifier", value: "createIdenifierElement" },
+    { name: "get Identity", value: "getIdentityElement" },
+    { name: "get contact identifier", value: "getContactIdentifierElement" },
+    { name: "update identity password", value: "updateIdentityPwdElement" }
   ]
-  elements=[]
-  selectedValue:string=""
-  show:boolean=false
-  attachForms=[]
-  attachedForms=[]
-  attachVal:string=""
-  attachedVal:string=""
-  attachTrack=[]
+  elements = []
+  selectedValue: string = ""
+  show: boolean = false
+  attachForms = []
+  attachedForms = []
+  attachVal: string = ""
+  attachedVal: string = ""
+  attachTrack = []
 
-  detachState:boolean=false
-  dettachedVal:string=""
-  dettachTracks=[]
-  identityBDay:any
+  detachState: boolean = false
+  dettachedVal: string = ""
+  dettachTracks = []
+  identityBDay: any
 
   //Create identity variables:
-  civilitys=[{
-    id:"1"
+  civilitys = [{
+    id: "1"
   },
   {
-    id:"2"
+    id: "2"
   },
   {
-    id:"3"
+    id: "3"
   }
 
-    
+
   ]
-  genders=[
+  genders = [
     {
-      name:"male",
-      id:"1"
+      name: "male",
+      id: "1"
     },
     {
-      name:"female",
-      id:"2"
+      name: "female",
+      id: "2"
     }
   ]
 
-  identifierTypes=[
+  identifierTypes = [
     {
-      name:"LOGIN",
+      name: "LOGIN",
     },
     {
-      name:"ISF",
+      name: "ISF",
     }
   ]
 
-  contactIdentifierTypes=[
+  contactIdentifierTypes = [
     {
-      name:"email",
+      name: "email",
     },
     {
-      name:"mobile",
+      name: "mobile",
     }
   ]
 
-  action:string=""
+  action: string = ""
+
+
+  contactIdentifiers: any[] = [];
+  myContactIdentifier: [{}];
 
 
   @ViewChild("parent", { static: false }) parentDiv: ElementRef
@@ -106,226 +110,193 @@ export class SgidfComponent implements OnInit, OnDestroy {
 
   constructor(
     private renderer: Renderer2,
-    private SGIDFSvc:SgidfService,
+    private SGIDFSvc: SgidfService,
     private formBuilder: FormBuilder
   ) { }
 
+
   // Create user -Full identity- forms
-  createFullIdentity=this.formBuilder.group({
-    status:['activated'],
-    profile:this.formBuilder.group({
-      civility:[],
-      displayName:['', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
-      firstName:['', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
-      lastName:['', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
-      avatar:["1'avatar1", [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
-      gender:['', [Validators.required, Validators.minLength(4), Validators.maxLength(6)]],
-      birthdate:['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
-      timeZone:['Europe/Paris'],
-      language:['fr']
+  createFullIdentity = this.formBuilder.group({
+    status: ['activated'],
+    profile: this.formBuilder.group({
+      civility: [],
+      displayName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+      firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+      lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+      avatar: ["1'avatar1", [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+      gender: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(6)]],
+      birthdate: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      timeZone: ['Europe/Paris'],
+      language: ['fr']
     }),
-    identifiers: this.formBuilder.array([
-      this.formBuilder.group({
-        provider:['sandbox'],
-        type:['LOGIN'],
-        value: ['bouthsayna.djebali@sofrecom.com']
-      })
+ /*    identifiers: this.formBuilder.array([{
+      // this.formBuilder.group({
+      provider: '',
+      type: '',
+      value: ''
+      // })
+    }
       // "provider": "sandbox",
       // "type": "LOGIN",
       // "value": "bouthayna.djebali@sofrecom.com"
-    ]),
-    password:this.formBuilder.group({
-      value:['', [Validators.required, Validators.minLength(3)]]
+    ]), */
+    password: this.formBuilder.group({
+      value: ['', [Validators.required, Validators.minLength(3)]]
     }),
-    // contactIdentifiers: this.formBuilder.group({
+
+    contactIdentifiers: this.formBuilder.array([]),
+    identifiers : this.formBuilder.array([])
+
+    /*  contactIdentifiers: this.formBuilder.array([{
+       value:'',
+       isPassword:'',
+       type:''
+     } */
+
+    /*     ]),
+     */    // contactIdentifiers: this.formBuilder.group({
     //   value:[''],
     //   isPassword:[false],
     //   type:['',[]],
     // }),
-    contactIdentifiers:  this.formBuilder.array([{
-        value:'bouthayna.djebali@sofrecom.com',
-        isPassword:false,
-        type:'email',
-      }])
+
+
+
   })
 
+  get getConcatIdentifiers() {
+    return this.createFullIdentity.controls["contactIdentifiers"] as FormArray;
+  }
+
+
+  get getIdentifiersForm() {
+    return this.createFullIdentity.controls["identifiers"] as FormArray;
+  }
+
+  /*    contactIdentifiers:  this.formBuilder.array([{
+         value:'omarthi@sofrecom.com',
+         isPassword:false,
+         type:'email',
+       }])
+   })
+ 
+  */
   // Create user -Full identity- forms
-  contactIdentifiersForms= this.formBuilder.group({
-    value:['', [Validators.required, Validators.email||Validators.minLength(8)]],
-    isPassword:[false],
-    type:['',[Validators.minLength(5), Validators.maxLength(6), Validators.required]],
+  contactIdentifiersForms = this.formBuilder.group({
+    value: ['', [Validators.required, Validators.email || Validators.minLength(8)]],
+    isPassword: [false],
+    type: ['', [Validators.minLength(5), Validators.maxLength(6), Validators.required]],
   })
 
-  // Create user -Full identity- forms
-  getIdentifier= this.formBuilder.group({
-    email:['', [Validators.required, Validators.email]],
+  IdentifiersForms = this.formBuilder.group({
+    type:  ['', [Validators.required, Validators.email || Validators.minLength(8)]],
+    value:  ['', [Validators.required, Validators.email || Validators.minLength(8)]],
+    provider: ['', [Validators.required, Validators.email || Validators.minLength(8)]],
+
   })
-  updateIdentityPwd=this.formBuilder.group({
-    password:this.formBuilder.group({
-      value:['', [Validators.required, Validators.minLength(8)]]
+
+
+  // Create user -Full identity- forms
+  getIdentifier = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+  })
+  updateIdentityPwd = this.formBuilder.group({
+    password: this.formBuilder.group({
+      value: ['', [Validators.required, Validators.minLength(8)]]
     }),
-    
+
 
   })
   ngOnInit() {
+    this.getConcatIdentifiers.push(this.contactIdentifiersForms);   
+    this.getIdentifiersForm.push(this.IdentifiersForms)
+
   }
 
   addNewCompany() {
     let control = <FormArray>this.createFullIdentity.controls.identifiers;
     control.push(
       this.formBuilder.group({
-        provider:[''],
-        type:[''],
+        provider: [''],
+        type: [''],
         value: ['']
       })
     )
   }
 
-  createNewUser(form:FormGroup){
-    // form.get("profile").get("civility").setValue(form.get("profile").get("gender").value)
-    // form.get("profile").get("civility").setValue(1);
-    this.createIdentityElementSbs=this.SGIDFSvc.creatIdentity(form.value).subscribe(
-      createIdentityRes=>{        
-      console.log(createIdentityRes)
-    },
-    (errorRes:HttpErrorResponse)=>{
-      console.log(errorRes)
-      if(errorRes.status==400){
-        this.error=true
-        this.errorMsg='Error creating user'
-      }else if(errorRes.status==500){
-        this.error=true
-        this.errorMsg="server error, please try later"
+  createNewUser(form: FormGroup) {
+
+    this.createIdentityElementSbs = this.SGIDFSvc.creatIdentity(form.value).subscribe(
+      createIdentityRes => {
+        console.log(createIdentityRes)
+      },
+      (errorRes: HttpErrorResponse) => {
+        console.log(errorRes)
+        if (errorRes.status == 400) {
+          this.error = true
+          this.errorMsg = 'Error creating user'
+        } else if (errorRes.status == 500) {
+          this.error = true
+          this.errorMsg = "server error, please try later"
+        }
       }
-    }
     )
   }
 
-  contactIdentifiersFn(form:FormGroup){
-    if( this.action!==""){
+  contactIdentifiersFn(form: FormGroup) {
+    if (this.action !== "") {
       this.SGIDFSvc.updateContactIdentifiers({
-        methode:this.action,
-        body:form.value
-      }).subscribe(contactIdentifiersFnRes=>{
+        methode: this.action,
+        body: form.value
+      }).subscribe(contactIdentifiersFnRes => {
         console.log(contactIdentifiersFnRes)
       })
-    }else{
-      this.error=true
-      this.errorMsg="Please choose action"
+    } else {
+      this.error = true
+      this.errorMsg = "Please choose action"
     }
   }
 
-  getIdentifierId(form:FormGroup){
-    this.SGIDFSvc.getIdentifier(form.value).subscribe(identifierRes=>{
+  getIdentifierId(form: FormGroup) {
+    this.SGIDFSvc.getIdentifier(form.value).subscribe(identifierRes => {
       console.log(identifierRes)
     },
-    (errorRes:HttpErrorResponse)=>{
-      console.log(errorRes)
-      if(errorRes.status==400){
-        this.error=true
-        this.errorMsg='Error identifier res'
-      }else if(errorRes.status==500){
-        this.error=true
-        this.errorMsg="server error, please try later"
-      }
-    })
+      (errorRes: HttpErrorResponse) => {
+        console.log(errorRes)
+        if (errorRes.status == 400) {
+          this.error = true
+          this.errorMsg = 'Error identifier res'
+        } else if (errorRes.status == 500) {
+          this.error = true
+          this.errorMsg = "server error, please try later"
+        }
+      })
   }
-  updatepwdIdentityFn(form:FormGroup){
+  updatepwdIdentityFn(form: FormGroup) {
     this.SGIDFSvc.updateidentitypwd(form.value).subscribe
   }
 
-  setAction(type){
-    this.action=type
+  setAction(type) {
+    this.action = type
   }
 
   addNewElem() {
-    // switch (this.selectedValue) {
-    //   case "getIdentifierElement":
-    //     this.renderer.setStyle(this.getIdentifierElement.nativeElement,"display", "block")
-    //     this.renderer.setStyle(this.getIdentifierElement.nativeElement,"position", "relative")
-    //     this.elements.push(`${this.getIdentifierElement}`)
-    //     this.attachForms.push({
-    //       name:"get identifier",
-    //       value:"getIdentifierElement"
-    //     })
-    //     this.attachedForms.push({
-    //       name:"get identifier",
-    //       value:"getIdentifierElement"
-    //     })
-    //     this.show=true
-    //     break;
-    //   case "createIdenifierElement":
-    //     this.renderer.setStyle(this.createIdenifierElement.nativeElement,"display", "block")
-    //     this.renderer.setStyle(this.createIdenifierElement.nativeElement,"position", "relative")
-    //     this.elements.push(`${this.createIdenifierElement}`)
-    //     this.attachForms.push({
-    //       name:"create identifier",
-    //       value:"createIdenifierElement"
-    //     })
-    //     this.attachedForms.push({
-    //       name:"create identifier",
-    //       value:"createIdenifierElement"
-    //     })
-    //     this.show=true
-    //     break;
-    //   case "getIdentityElement":
-    //     this.renderer.setStyle(this.getIdentityElement.nativeElement,"display", "block")
-    //     this.renderer.setStyle(this.getIdentityElement.nativeElement,"position", "relative")
-    //     this.elements.push(`${this.getIdentityElement}`)
-    //     this.attachForms.push({
-    //       name:"get identity",
-    //       value:"getIdentityElement"
-    //     })
-    //     this.attachedForms.push({
-    //       name:"get identity",
-    //       value:"getIdentityElement"
-    //     })
-    //     this.show=true
-    //     break;
-    //   case "getContactIdentifierElement":
-    //     this.renderer.setStyle(this.getContactIdentifierElement.nativeElement,"display", "block")
-    //     this.renderer.setStyle(this.getContactIdentifierElement.nativeElement,"position", "relative")
-    //     this.elements.push(`${this.getContactIdentifierElement}`)
-    //     this.attachForms.push({
-    //       name:"get contact identifier",
-    //       value:"getContactIdentifierElement"
-    //     })
-    //     this.attachedForms.push({
-    //       name:"get contact identifier",
-    //       value:"getContactIdentifierElement"
-    //     })
-    //     this.show=true
-    //     break;
-    //   case "updateIdentityPwdElement":
-    //     this.renderer.setStyle(this.updateIdentityPwdElement.nativeElement,"display", "block")
-    //     this.renderer.setStyle(this.updateIdentityPwdElement.nativeElement,"position", "relative")
-    //     this.elements.push(`${this.updateIdentityPwdElement}`)
-    //     this.attachForms.push({
-    //       name:"update identity",
-    //       value:"updateIdentityPwdElement"
-    //     })
-    //     this.attachedForms.push({
-    //       name:"update identity",
-    //       value:"updateIdentityPwdElement"
-    //     })
-    //     this.show=true
-    //     break;
-    // }
+  
   }
 
-  updateAttachVal(val){
-    this.attachVal=val
+  updateAttachVal(val) {
+    this.attachVal = val
   }
 
-  updateDettachedVal(val){
-    this.dettachedVal=val
+  updateDettachedVal(val) {
+    this.dettachedVal = val
   }
 
-  updateAttachedVal(val){
-    this.attachedVal=val
+  updateAttachedVal(val) {
+    this.attachedVal = val
   }
 
-  nameChanged(event){
+  nameChanged(event) {
     console.log(event)
 
   }
@@ -333,11 +304,11 @@ export class SgidfComponent implements OnInit, OnDestroy {
   updatePosition() {
     for (const track of this.attachTrack) {
       this.line[track.index].remove()
-      this.line[track.index]=new LeaderLine(track.attach.nativeElement, track.attached.nativeElement, {color: 'black', size: 4, endLabel: `${track.link}`});
+      this.line[track.index] = new LeaderLine(track.attach.nativeElement, track.attached.nativeElement, { color: 'black', size: 4, endLabel: `${track.link}` });
     }
   }
 
-  attach(){
+  attach() {
     // if(this.attachVal==null || this.attachVal==undefined || this.attachedVal==null || this.attachedVal==undefined){
     //   this.error=true
     //   this.errorMsg="Please choose form before click on attach button"
@@ -485,7 +456,7 @@ export class SgidfComponent implements OnInit, OnDestroy {
     // }
   }
 
-  detach(){
+  detach() {
     // if (this.dettachedVal!=null ||this.dettachedVal!=undefined ) {
     //   this.attachTrack=this.attachTrack.filter(elem=>elem.link!=this.dettachedVal)
     //   const index=this.dettachedVal.split(' ')[1]
